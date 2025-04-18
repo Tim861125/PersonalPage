60a6cf31eb6f1e1abfafb97ced109d80ce25a33a
Tim861125 <tim861125@gmail.com>
2025-04-18 12:33:40 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
-
+import './App.css'
 export default function PersonalWebsite() {
   const [darkMode, setDarkMode] = useState(false);
 
